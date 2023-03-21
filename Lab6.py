621134a5890fb7970d2435216caa831e45eafb91
Dane314pizza <83150517+Dane314pizza@users.noreply.github.com>
2023-03-21 17:12:33 -0400

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,29 @@
+# Lab 6
+# Author: Dane Dickerson
+
+def encode(password):
+    password_encoded = []
+    for letter in password:
+        new_letter = str((int(letter)+3)%10)
+        password_encoded.append(new_letter)
+        
+    password_encoded = ''.join(password_encoded)
+    return password_encoded
+
+
+if __name__ == '__main__':
+    while True:
+        print('Menu')
+        print('-'*13)
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+        option = input('Please enter an option: ')
+        if option == '1':
+            password = input('Please enter a password to encode: ')
+            password_encoded = encode(password)
+            print('Your password has been encoded and stored!\n')
+        elif option == '2':
+            print(f'The encoded password is {password_encoded}, and the original password is {password}.\n')
+        elif option == '3':
+            break
